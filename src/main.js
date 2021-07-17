a307eefe5e8ccd9fc1cb80e5b82e673fd207d0c1
Tahmina Akhtar <tahminausa90@gmail.com>
2021-07-17 22:52:03 +0600

    Vue Router

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  Router,
+  components: { App },
+  template: '<App/>'
+});
